68be6ffd0aefc9b301087e9a51d52057c4f63e62
DataNath <Nathan.p100@hotmail.co.uk>
2024-11-24 23:40:34 +0000

    Links added, GUI finished, bundled

--- bundle/gui.py
+++ bundle/gui.py
@@ -1,4 +1,4 @@
-import os, sys, subprocess, threading, customtkinter as ctk
+import os, sys, subprocess, threading, webbrowser, customtkinter as ctk
 
 def init_new_thread():
     cookie = enter_cookie.get()
@@ -22,7 +22,10 @@ def run_main(cookie):
 
     try:
         message = subprocess.run(
-            ["python", main_path, template_path, cookie], capture_output=True, text=True
+            ["python", main_path, template_path, cookie],
+            capture_output=True,
+            text=True,
+            creationflags=subprocess.CREATE_NO_WINDOW,
         )
         response.insert(ctk.END, message.stdout)
         response.see(ctk.END)
@@ -31,14 +34,23 @@ def run_main(cookie):
 
     response.configure(state="disabled")
 
+def launch_alteryx():
+    webbrowser.open_new_tab("https://community.alteryx.com/t5/Alter-Nation/Advent-of-Code-2024/ba-p/1342296")
+
+def launch_github():
+    webbrowser.open_new_tab("https://github.com/DataNath/AoC_startFile_generator")
+
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
 
 root = ctk.CTk()
-root.geometry("750x600")
+root.geometry("650x700")
+root.resizable(False, False)
 root.title("Alteryx Advent of Code start file generator")
 
-frame = ctk.CTkFrame(master=root)
+bg_colour = "#2b2b2b"
+
+frame = ctk.CTkFrame(master=root, fg_color=bg_colour)
 frame.pack(pady=10, padx=10, fill="both", expand=True)
 
 main_label = ctk.CTkLabel(
@@ -57,16 +69,42 @@ info_label = ctk.CTkLabel(
 )
 info_label.pack(pady=12, padx=10)
 
+button_frame = ctk.CTkFrame(master=frame, height=30, width=320, fg_color=bg_colour)
+button_frame.pack(
+    pady=10,
+    padx=10,
+    anchor="center")
+
+alteryx_button = ctk.CTkButton(
+    master=button_frame,
+    text="Additional info",
+    height=30,
+    width=150,
+    font=("Aptos", 18),
+    command=launch_alteryx
+)
+alteryx_button.pack(side="left", padx=5)
+
+github_button = ctk.CTkButton(
+    master=button_frame,
+    text="GitHub repo",
+    height=30,
+    width=150,
+    font=("Aptos", 18),
+    command=launch_github
+)
+github_button.pack(side="right", padx=5)
+
 enter_cookie = ctk.CTkEntry(
     master=frame,
     placeholder_text="Enter session cookie",
     height=30,
-    width=300,
+    width=310,
     justify="center",
     font=("Aptos", 18),
     show="*",
 )
-enter_cookie.pack(pady=12, padx=5)
+enter_cookie.pack(pady=12, padx=10)
 
 button = ctk.CTkButton(
     master=frame,
@@ -79,7 +117,7 @@ button = ctk.CTkButton(
 button.pack(pady=12, padx=5)
 
 response = ctk.CTkTextbox(
-    master=frame, state="disabled", height=200, width=500, font=("Aptos", 18)
+    master=frame, state="disabled", height=400, width=500, font=("Aptos", 18)
 )
 response.pack(pady=12, padx=5)
 
